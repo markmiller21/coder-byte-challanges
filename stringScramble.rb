edb399ca2d9efac2ce6e822c6b0725ee7ec6d095
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-11-17 16:56:59 -0500

    add refactored solution ot StringScramble

--- stringScramble.rb
+++ stringScramble.rb
@@ -1,14 +1,18 @@
+# def StringScramble(str1,str2)
+# 	answer = true
+#   for x in 0..str2.length-1
+#   	if str1.include? str2[x]
+#   		p "nothing"
+#   	else
+#   		answer = false
+#   	end
+#   end
+#   return answer      
+# end
+
 def StringScramble(str1,str2)
-	answer = true
-  for x in 0..str2.length-1
-  	if str1.include? str2[x]
-  		p "nothing"
-  	else
-  		answer = false
-  	end
-  end
-  return answer 
-         
+  str2.split("").each {|letter| return false if str1.slice!(letter) != letter}
+  return true
 end
 
 StringScramble('cdore', 'coder')
