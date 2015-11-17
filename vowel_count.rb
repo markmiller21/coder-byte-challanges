2e894dbeafad429b4ee1184b541258fd5e65c3ef
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-11-17 13:19:50 -0500

    change vowel array to string for faster checking

--- vowel_count.rb
+++ vowel_count.rb
@@ -1,5 +1,5 @@
 def VowelCount(str)
-  vowels = ["a", "e", "i", "o", "u"]
+  vowels = 'aeiouAEIOU'
   letter_array = str.split('')
   count = 0
   letter_array.each { |letter| count += 1 if vowels.include? letter }
