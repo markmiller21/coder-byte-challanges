484939da69560f7a7718ced6d4c449686f53ede4
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-11-17 16:16:38 -0500

    add example for furture clarification

--- meanMode.rb
+++ meanMode.rb
@@ -10,6 +10,9 @@ end
 
 def mode(array)
 	mode_array = array.inject({}) { |k, v| k[v] = array.count(v); k}
+	p mode_array
 	mode_array.select { |k,v| v == mode_array.values.max }.keys
 end
+
+MeanMode([7,7,7,4,4,5,4,5,7,2,2,3,3,7,3,4])
 
