c100a84daf4cdee037f2aa46e47f2f26b01b898f
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-11-17 15:53:11 -0500

    add refactored solution

--- secondGreatLow.rb
+++ secondGreatLow.rb
@@ -1,8 +1,9 @@
 def SecondGreatLow(arr)
   arr = arr.uniq.sort
   result_arr = []
-  result_arr << arr[1]
-  result_arr << arr[arr.length-2]
-  return result_arr.join(' ') 
-         
+  result_arr.push(arr[1])
+  result_arr.push(arr[arr.length-2])
+  return result_arr.join(' ')       
 end
+
+SecondGreatLow([1,2,3,4,5,6])
