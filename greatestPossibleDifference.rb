562f8514f396ec183268edb6c87fb4c85463cb2c
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-11-18 12:24:42 -0500

    refactor greatest possible difference code

--- greatestPossibleDifference.rb
+++ greatestPossibleDifference.rb
@@ -1,8 +1,18 @@
+# def GreatestPossibleDifference(array)
+# 	array = array.sort!
+# 	difference = array[-1] - array[0]
+# end
+
 def GreatestPossibleDifference(array)
-	array = array.sort!
-	difference = array[-1] - array[0]
+	max = array[0]
+	min = array[0]
+	array.each do |number|
+		max = number if number > max
+		min = number if number < min
+	end
+	difference = max - min
 end
 
 
 p GreatestPossibleDifference([1,9,2,3,4,5,6])
-p GreatestPossibleDifference([1])
+p GreatestPossibleDifference([10,-2])
